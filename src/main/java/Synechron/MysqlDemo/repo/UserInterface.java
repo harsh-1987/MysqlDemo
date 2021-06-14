c2ef4af52a9deec81937ea6853f4e1b6abd87880
Harsh Mishra <contactharshmishra@gmail.com>
2021-06-14 16:28:09 +0530

    addede annotation

--- src/main/java/Synechron/MysqlDemo/repo/UserInterface.java
+++ src/main/java/Synechron/MysqlDemo/repo/UserInterface.java
@@ -1,9 +1,11 @@
 package Synechron.MysqlDemo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import Synechron.MysqlDemo.model.User;
 
+@Repository
 public interface UserInterface extends JpaRepository<User, Long>{
 
 }
